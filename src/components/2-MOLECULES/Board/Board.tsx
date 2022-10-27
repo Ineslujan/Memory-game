ddb10403823702f77bdbefd6793e2ba75c28008b
Inès LUJAN <melusyne@iness-macbook-air.home>
2022-10-27 18:13:05 +0200

    Cards shuffle Done

--- src/components/2-MOLECULES/Board/Board.tsx
+++ src/components/2-MOLECULES/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import data from '../../../datas/cards.json';
 import { CardType } from '../../../interfaces/Cards';
@@ -7,9 +7,18 @@ import Card from '../../1-ATOMS/Card/Card';
 import './Board.scss';
 
 function Board() {
+  const [shuffledCards, setShuffledCards] = useState<CardType[]>([]);
+
+  useEffect(() => {
+    const dataToSort = [...data.cards];
+    const shuffledDatas = dataToSort.sort(() => Math.random() - 0.5);
+    console.log(data.cards, shuffledDatas);
+    setShuffledCards([...shuffledDatas]);
+  }, []);
+
   return (
     <section className="board">
-      {data.cards.map((card: CardType) => <Card key={card.id} cardName={card.name} />)}
+      {shuffledCards.map((card: CardType) => <Card key={card.id} cardName={card.name} />)}
     </section>
   );
 }
